a21a6929166e6b6d6d73c5ed22e8d3659c4bff14
Oliver Trosien <otrosien@epages.com>
2015-11-16 16:03:16 +0100

    alpn-boot WIP (1)

--- src/main/java/jettyproxy/ProxyServer.java
+++ src/main/java/jettyproxy/ProxyServer.java
@@ -8,22 +8,24 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 public class ProxyServer {
 
-	public static void main(String[] args) throws Exception {
-		Server server = new Server(3128);
-		ConnectHandler handler = new org.eclipse.jetty.proxy.ConnectHandler();
-		handler.setServer(server);
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(3128);
+        ConnectHandler handler = new org.eclipse.jetty.proxy.ConnectHandler();
+        handler.setServer(server);
         HandlerCollection handlers = new HandlerCollection();
         server.setHandler(handlers);
         ServletContextHandler context = new ServletContextHandler(handlers, "/");
         ServletHolder proxyServlet = new ServletHolder("proxyHolder", org.eclipse.jetty.proxy.ProxyServlet.class);
-//        proxyServlet.setInitParameter("whiteList", "google.com, www.eclipse.org, localhost");
-//        proxyServlet.setInitParameter("blackList", "google.com/calendar/*, www.eclipse.org/committers/");
-        context.addServlet(proxyServlet, "/*");		
+        // proxyServlet.setInitParameter("whiteList", "google.com,
+        // www.eclipse.org, localhost");
+        // proxyServlet.setInitParameter("blackList", "google.com/calendar/*,
+        // www.eclipse.org/committers/");
+        context.addServlet(proxyServlet, "/*");
         ConnectHandler proxy = new ConnectHandler();
         handlers.addHandler(proxy);
 
-		server.start();
-		server.join();
-	}
+        server.start();
+        server.join();
+    }
 
 }
