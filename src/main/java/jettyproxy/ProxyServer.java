4f82aaa1c1b4b9bada1e9fef2e2f1d77646c3ec0
Oliver Trosien <otrosien@epages.com>
2015-11-17 16:00:27 +0100

    h2c-based http/2 proxy

--- src/main/java/jettyproxy/ProxyServer.java
+++ src/main/java/jettyproxy/ProxyServer.java
@@ -1,7 +1,11 @@
 package jettyproxy;
 
+import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory;
 import org.eclipse.jetty.proxy.ConnectHandler;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.ProxyConnectionFactory;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -10,16 +14,15 @@ public class ProxyServer {
 
     public static void main(String[] args) throws Exception {
         Server server = new Server(3128);
-        ConnectHandler handler = new org.eclipse.jetty.proxy.ConnectHandler();
-        handler.setServer(server);
+        HttpConfiguration configuration = new HttpConfiguration();
+        ServerConnector connector = new ServerConnector(server, new ProxyConnectionFactory(),
+                new HTTP2CServerConnectionFactory(configuration));
+        server.addConnector(connector);
+
         HandlerCollection handlers = new HandlerCollection();
         server.setHandler(handlers);
         ServletContextHandler context = new ServletContextHandler(handlers, "/");
         ServletHolder proxyServlet = new ServletHolder("proxyHolder", org.eclipse.jetty.proxy.ProxyServlet.class);
-        // proxyServlet.setInitParameter("whiteList", "google.com,
-        // www.eclipse.org, localhost");
-        // proxyServlet.setInitParameter("blackList", "google.com/calendar/*,
-        // www.eclipse.org/committers/");
         context.addServlet(proxyServlet, "/*");
         ConnectHandler proxy = new ConnectHandler();
         handlers.addHandler(proxy);
